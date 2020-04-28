1cd1603ad6df6573476464d87a4fb5296246ee71
Armin Steinhauser <armin.steinhauser@kuleuven.be>
2020-04-28 15:09:56 +0200

    add some first top-level documentation

--- sympathor/__init__.py
+++ sympathor/__init__.py
@@ -1,1 +1,17 @@
-from sympathor.parser import ParsePaths
+"""sympathor brings differential geometry to SVG paths.
+
+sympathor enables you to extract a fully symbolic description of paths from \
+MIME types `text/plain` or `image/svg+xml`. Based on this symbolic \
+description, you can obtain a sampled path for further numerical processing, \
+the path's natural parametrization, its Frenet frame, etc.
+
+  Typical usage example:
+
+  .. code-block:: python
+
+      path = ParsePaths('path/to/file.svg')[0]
+      t = path.tangent()
+      c = path.curvature()
+"""
+
+from sympathor.parser import ParsePaths  # noqa
