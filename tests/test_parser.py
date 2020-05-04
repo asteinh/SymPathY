52cca41baf09c65914f01cc58d92adcc365dc15f
Armin Steinhauser <armin.steinhauser@kuleuven.be>
2020-05-04 07:05:47 +0200

    more tests for parser; increasing coverage

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,16 +1,16 @@
 import pytest
 from sympathor.parser import ParsePaths
-# import casadi as cas
-# import copy
 
 
 class TestParser():
     @pytest.fixture(params=[
-        ('tests/files/test_ok_1.svg', True),
+        ('tests/files/test_ok_1.svg', True),  # multiple paths
         ('tests/files/test_ok_2.svg', True),  # no paths
         ('tests/files/test_ok_3.svg', True),  # no svg tag
         ('tests/files/test_ok_4.svg', True),  # no xml tag
+        ('tests/files/test_ok_5.svg', True),  # plain path
         ('tests/files/test_not_ok_1.svg', False),  # no namespace
+        ('tests/files/test_not_ok_2.svg', False),  # parse error
         ('M 0 0 C 10,10 20,10 30,0 Z', True),
     ])
     def source(self, request):
@@ -20,7 +20,7 @@ class TestParser():
         if source['valid']:
             paths = ParsePaths(source['str'])
             if len(paths) > 0:
-                p = paths[0]
+                assert paths[0]
         else:
             with pytest.raises(ValueError):
                 paths = ParsePaths(source['str'])
